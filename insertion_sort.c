ca4429c34db10a0c03b282e65b6ea73b4e9813d2
smsn <rootsmsn@gmail.com>
2019-06-08 14:45:00 +0800

    插入排序

--- insertion_sort.c
+++ insertion_sort.c
@@ -1,27 +1,22 @@
 #include <stdio.h>
 
-void display(int arr[], int size)
-{
-    for (int i = 0; i < size; i++)
-    {
+void display(int arr[], int size) {
+    for (int i = 0; i < size; i++) {
         printf("%d ", arr[i]);
     }
     printf("\n");
 }
 
-void insertion_sort(int arr[], int size)
-{
-    for (int i = 1; i < size; i++)
-    // 从第二个开始
-    {
+void insertion_sort(int arr[], int size) {
+    for (int i = 1; i < size; i++) {
+        // 从第二个开始
         int temp = arr[i];
         printf("get %d\n", arr[i]);
         int j;
-        for (j = i; j > 0 && arr[j - 1] > temp; j--)
-        // 与arr[i]前面的比较
-        {
+        for (j = i; j > 0 && arr[j - 1] > temp; j--) {
+            // 与arr[i]前面的比较
             // arr[i-1] 后移
-            printf("%d 后移:     ", arr[j-1]);
+            printf("%d 后移:     ", arr[j - 1]);
             arr[j] = arr[j - 1];
             display(arr, size);
         }
@@ -32,8 +27,7 @@ void insertion_sort(int arr[], int size)
     }
 }
 
-int main(int argc, char const *argv[])
-{
+int main(int argc, char const *argv[]) {
     int n;
     printf("Enter size of array:\n");
     scanf("%d", &n);
@@ -41,8 +35,7 @@ int main(int argc, char const *argv[])
     printf("Enter the elements of the array:\n");
     int i;
     int arr[n];
-    for (i = 0; i < n; i++)
-    {
+    for (i = 0; i < n; i++) {
         scanf("%d", &arr[i]);
     }
 
